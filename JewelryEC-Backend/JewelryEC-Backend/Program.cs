d32852042b7fe812f3a7e137930dcee05d38931d
Thuy Trinh ne <21522719@gm.uit.edu.vn>
2024-04-15 14:44:40 +0700

    add

--- JewelryEC-Backend/JewelryEC-Backend/Program.cs
+++ JewelryEC-Backend/JewelryEC-Backend/Program.cs
@@ -1,18 +1,10 @@
 using FluentValidation.WebApi;
 using JewelryEC_Backend.Data;
-using JewelryEC_Backend.Extensions;
 using JewelryEC_Backend.Filters;
-using JewelryEC_Backend.Models.Auths;
-using JewelryEC_Backend.Models.Auths.Entities;
-using JewelryEC_Backend.Service;
-using JewelryEC_Backend.Service.IService;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,62 +13,19 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
-builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-builder.Services.AddScoped<IEmailSender, EmailSender>();
-builder.Services.AddScoped<IRedisShoppingCartService, RedisShoppingCartService>();
-
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add(new ValidateModelAttribute());
 });
 
 
-
-
 builder.Services.AddControllers();
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(option =>
-{
-    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference= new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id=JwtBearerDefaults.AuthenticationScheme
-                }
-            }, new string[]{}
-        }
-    });
-});
-#region authentication 
-// when we are authenticating, we are validating 3 things
-// (secret,issuer, audience )
-// => we will have to validate our token using all 3 of them
-#endregion
-builder.AddAppAuthetication();
-builder.Services.AddAuthorization();
-
+builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
@@ -92,9 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
+
 app.UseAuthorization();
-app.UseAuthentication();
 
 app.MapControllers();
 
